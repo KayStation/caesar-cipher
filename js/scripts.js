993210b35c8682d6c564395ae2acff98e4709335
KayStation <matt0122@comcast.net>
2022-01-29 15:57:52 -0600

    Added variables connected to HTML

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,22 @@
+const phrase = document.getElementById('phrase');
+const key = document.getElementById('key');
+const form = document.getElementById('form');
+
+form.addEventListener('submit', (e) => {
+    let mess
+})
+
+
+
+
+
+
+
+
+
+
+
+
 const caesar = function(str, key) {
     const upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
     const lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
